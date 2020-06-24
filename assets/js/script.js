f191f59d99b49ab0606683b2d3ee5bff28e8f22d
Waves <64664814+sanders43@users.noreply.github.com>
2020-06-24 00:20:23 -0500

    refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,29 +2,41 @@ const formEl = document.querySelector("#task-form");
 let tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
-const createTaskHandler = (event) => {
+const taskFormHandler = (event) => {
 
     event.preventDefault();
     let taskNameInput = document.querySelector("input[name='task-name']").value;
     let taskTypeInput = document.querySelector("select[name='task-type']").value;
-    
-    // create list item
-    let listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
+    let taskDataObj = {
+        // package data as object
+        name: taskNameInput,
+        type: taskTypeInput
+    };
 
-    // create div to hold task info and add to list item
-    let taskInfoEl = document.createElement("div");
-    //assign class name
-    taskInfoEl.className = "task-info";
-    // add html content to div
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    //send as an argument
+    createTaskEl(taskDataObj);
+    
+    
+};
 
-    listItemEl.appendChild(taskInfoEl);
-    
-    
-    tasksToDoEl.appendChild(listItemEl);
+const createTaskEl = (taskDataObj) => {
+// create list item
+let listItemEl = document.createElement("li");
+listItemEl.className = "task-item";
+
+// create div to hold task info and add to list item
+let taskInfoEl = document.createElement("div");
+//assign class name
+taskInfoEl.className = "task-info";
+// add html content to div
+taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+
+listItemEl.appendChild(taskInfoEl);
+
+
+tasksToDoEl.appendChild(listItemEl);
 };
 
 
-formEl.addEventListener("submit",createTaskHandler);
+formEl.addEventListener("submit",taskFormHandler);
     
