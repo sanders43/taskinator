0de795a15acd7b642749836304d6aa5eb0d090cc
Waves <64664814+sanders43@users.noreply.github.com>
2020-06-24 00:26:40 -0500

    reset form and invalid entry

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,15 @@ const taskFormHandler = (event) => {
     event.preventDefault();
     let taskNameInput = document.querySelector("input[name='task-name']").value;
     let taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    };
+
+    formEl.reset();
+
+
     let taskDataObj = {
         // package data as object
         name: taskNameInput,
