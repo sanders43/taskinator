a9b3dcd6547f40bb2deaf206434f90527356842d
Waves <64664814+sanders43@users.noreply.github.com>
2020-06-24 00:09:39 -0500

    text and type both saing to browser

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,23 @@ let tasksToDoEl = document.querySelector("#tasks-to-do");
 const createTaskHandler = (event) => {
 
     event.preventDefault();
-
-
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    // create list item
     let listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // create div to hold task info and add to list item
+    let taskInfoEl = document.createElement("div");
+    //assign class name
+    taskInfoEl.className = "task-info";
+    // add html content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+    
+    
     tasksToDoEl.appendChild(listItemEl);
 };
 
